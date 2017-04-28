68822a7f53d388cbf0ad0b6d04844153ed278c84
Max Aldunate <maxaldunate@gmail.com>
2017-04-28 17:53:41 +0200

    adding pre refeactored

---
+++ StrategyConsoleRefactored/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TallerOop.StrategyConsole
+{
+    public class Cliente
+    {
+        public Cliente(string nif, string nombre, bool esGranCuenta, bool esMoroso, bool esSocio)
+        {
+            Nif = nif;
+            Nombre = nombre;
+            EsGranCuenta = esGranCuenta;
+            EsMoroso = esMoroso;
+            EsSocio = esSocio;
+        }
+
+        public string Nif { set; get; }
+        public string Nombre { set; get; }
+        public bool EsGranCuenta { set; get; }
+        public bool EsMoroso { set; get; }
+        public bool EsSocio { set; get; }
+    }
+}
---
+++ StrategyConsoleRefactored/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TallerOop.StrategyConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var paq1 = new Paquete(10, 10, 15, 20);
+            var cli = new Cliente("NIF1", "Max", true, true, true);
+
+            var gestor = new GestorDescuentos(cli);
+            gestor.SetPrecio(paq1);
+            Console.WriteLine("Precio Moroso      = " + paq1.Precio);
+
+            cli.EsMoroso = false;
+            gestor.SetPrecio(paq1);
+            Console.WriteLine("Precio Socio       = " + paq1.Precio);
+
+            cli.EsSocio = false;
+            gestor.SetPrecio(paq1);
+            Console.WriteLine("Precio Gran Cuenta = " + paq1.Precio);
+
+            cli.EsGranCuenta = false;
+            gestor.SetPrecio(paq1);
+            Console.WriteLine("Precio General     = " + paq1.Precio);
+
+            Console.ReadKey();
+        }
+    }
+}
