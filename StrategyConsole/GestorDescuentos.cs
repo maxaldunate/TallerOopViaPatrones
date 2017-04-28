98bdbffb9ce817a66767b3363b44b74a55194ca3
Max Aldunate <maxaldunate@gmail.com>
2017-04-28 17:50:29 +0200

    adding strategy no refactored

---
+++ StrategyConsole/GestorDescuentos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TallerOop.StrategyConsole
+{
+    public class GestorDescuentos
+    {
+        private Cliente _cliente;
+
+        public GestorDescuentos(Cliente cliente)
+        {
+            _cliente = cliente;
+        }
+
+        public void SetPrecio(Paquete paquete)
+        {
+            if (_cliente.EsMoroso)
+                paquete.Precio = -1;
+            else if (_cliente.EsSocio)
+                paquete.Precio = (paquete.Alto + 1);
+            else if (_cliente.EsGranCuenta)
+                paquete.Precio = (paquete.Alto + paquete.Peso);
+            else
+                paquete.Precio = (paquete.Alto * paquete.Peso);
+        }
+
+    }
+}
---
+++ StrategyConsole/Paquete.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TallerOop.StrategyConsole
+{
+    public class Paquete
+    {
+        public Paquete(double peso, double alto, double ancho, double largo)
+        {
+            Alto = alto;
+            Ancho = ancho;
+            Largo = largo;
+            Peso = peso;
+        }
+
+        public double Alto { private set; get; }
+        public double Ancho { private set; get; }
+        public double Largo { private set; get; }
+        public double Peso { private set; get; }
+
+        public double Precio { set; get; }
+
+        public override string ToString()
+        {
+            return Peso + " Kg. Dim=" + Alto + "x" + Ancho + "x" + Largo + ".";
+        }
+
+    }
+}
